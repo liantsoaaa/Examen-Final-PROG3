27e11d63c2ff8624cca0f5f8c1104aede876d66d
Lantosoa <lantosoanir@gmail.com>
2026-04-21 13:56:20 +0200

    feat: add OpenAPI for Agricultural federation API , section A to G

---
+++ src/main/java/com/hei/openapi_federation/OpenApiFederationApplication.java
@@ -0,0 +1,13 @@
+package com.hei.openapi_federation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenApiFederationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenApiFederationApplication.class, args);
+	}
+
+}
